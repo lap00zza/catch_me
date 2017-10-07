f0eebd4c6136d06d28b3d27aabc7e59c67bece4e
Jewel Mahanta <jewelmahanta@gmail.com>
2017-10-07 12:40:21 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+/* --- Chronometer --- */
+// Chronometer variables
+let clear_time;
+let milliseconds = 0, seconds = 0, minutes = 0, hours = 0;
+let chron_millis, chron_secs, chron_mins, chron_hours;
+
+const start_chron = () => {
+    // Formatting milliseconds to show in chronometer
+    chron_millis = (milliseconds < 1000) ? ("0" + milliseconds) : (milliseconds);
+    if (milliseconds === 1000) {
+        milliseconds = 0;
+        seconds++;
+    }
+    // Formatting seconds to show in chronometer
+    chron_secs = (seconds < 10) ? ("0" + seconds + " : ") : (seconds + " : ");
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+    // Formatting minutes to show in chronometer
+    chron_mins = (minutes < 10) ? ("0" + minutes + " : ") : (minutes + " : ");
+    if (minutes === 60) {
+        minutes = 0;
+        hours++;
+    }
+    // Formatting hours to show in chronometer
+    chron_hours = (hours < 10) ? ("0" + hours + " : ") : (hours + " : ");
+
+    // Display the chronometer
+    const chron = document.getElementById("chronometer");
+    chron.innerHTML = "Time: " + chron_hours + chron_mins + chron_secs + chron_millis;
+    milliseconds += 100;
+    clear_time = setTimeout(start_chron, 100);
+};
+
+const reset_chron = () => {
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+};
+
+const stop_chron = () => {
+    clearTimeout(clear_time)
+};
+/* ---~~~--- */
+
 // Available difficulties
 const diff_enum = {
     EASY: 0,
@@ -5,6 +52,11 @@ const diff_enum = {
     HARD: 50,
     EXTREME: 100,
 };
+
+//Setting difficulty from url
+const url = new URL(window.location);
+let diff_param = url.searchParams.get("difficulty");
+diff_param =  diff_param ? diff_param.toUpperCase() : "HARD";
 
 // Utility Functions
 const mv_x = (what, x) => what.style.left = x + "px",
@@ -15,13 +67,15 @@ const mv_x = (what, x) => what.style.left = x + "px",
 // Selectors. Duh!
 const catch_me = document.getElementById("catch_me"),
     counter = document.getElementById("counter"),
-    body = document.querySelectorAll(".body")[0];
+    body = document.querySelectorAll(".body")[0],
+    diff_btns = document.querySelectorAll(".diff-btn"),
+    active_diff_btn = document.getElementById(diff_param);
 
 // Useful global values
 const catch_height = 50,
     catch_width = 100,
-    difficulty = diff_enum.HARD; // CHANGE THIS FOR MORE FUN
-
+    difficulty = diff_enum[diff_param];
+    
 const incr_counter = () => counter.innerText = +counter.innerText + 1;
 // Next positions should be within the visible portion of the window.
 const rand_x = () => rand(window.innerWidth - catch_width),
@@ -30,6 +84,8 @@ const rand_x = () => rand(window.innerWidth - catch_width),
     mv_catch_y = () => mv_y(catch_me, rand_y());
 
 // Initial State
+active_diff_btn.setAttribute("disabled", "disabled");
+start_chron();
 catch_me.style.position = "absolute";
 catch_me.style.height = catch_height + "px";
 catch_me.style.width = catch_width + "px";
@@ -43,11 +99,6 @@ catch_me.addEventListener("mouseenter", () => {
     mv_catch_x();
     mv_catch_y();
     incr_counter();
-    if (counter.innerHTML % 10 === 0) {
-        counter.style.fontSize = (counter.innerHTML) * 3 + "pt";
-        counter.style.width = (counter.innerHTML) * 4 + "pt";
-        counter.style.height = (counter.innerHTML) * 4 + "pt";
-    }
 });
 
 // Track mouse movement inside body.
@@ -61,4 +112,11 @@ body.addEventListener("mousemove", e => {
         mv_catch_y();
         // console.log(`x: ${delta_x} y: ${delta_y}`);
     }
+});
+
+diff_btns.forEach(el => {
+    el.addEventListener("click", e => {
+        url.searchParams.set("difficulty", e.target.id);
+        window.location.replace(url);
+    })
 });
